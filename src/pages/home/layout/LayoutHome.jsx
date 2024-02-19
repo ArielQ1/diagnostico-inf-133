efe63e128f579d24acafe3d4af247ef4dacfd308
ArielQ1 <quizayaariel@gmail.com>
2024-02-19 10:18:42 -0400

    formato del proyecto, pre final

--- src/pages/home/layout/LayoutHome.jsx
+++ src/pages/home/layout/LayoutHome.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import css from './layout.module.scss';
-import Header from '../header/Header';
 import axios from 'axios';
 import { URL_POKEMON } from '../../../api/apiRest';
 import Card from '../card/Card';
@@ -17,21 +16,9 @@ export default function LayoutHome() {
 
         api();
     }, []);
-
- /*   useEffect(() => {    
-    
-        const api = async () => {
-          const apiPoke = await axios.get(${URL_POKEMON}/?offset=0&limit=${limit});
-          setArrayPokemon(apiPoke.data.results);
-        }
-        
-        api();
-      }, []);*/
-
     
   return (
     <div className={css.layout}>
-        <Header/>
         <div className={css.card_content}>
 
             {arrayPokemon.slice(0,45).map((card, index) => (
